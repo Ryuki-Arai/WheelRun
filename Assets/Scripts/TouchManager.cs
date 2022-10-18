a98f7a9923f80b44570a18a60a07689dbae2a2ab
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2022-10-18 16:49:32 +0900

    Import TouchScript, Used Flick

--- Assets/Scripts/TouchManager.cs
+++ Assets/Scripts/TouchManager.cs
@@ -1,8 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TouchScript.Gestures;
 using UnityEngine;
 
 public class TouchManager : MonoBehaviour
 {
-    
+    public FlickGesture flickGesture;
+
+    private void OnEnable()
+    {
+        flickGesture.Flicked += OnFlicked;
+    }
+
+    private void OnDisable()
+    {
+        flickGesture.Flicked -= OnFlicked;
+    }
+
+    private void OnFlicked( object sender, EventArgs e )
+    {
+        Debug.Log( "ƒtƒŠƒbƒN‚³‚ê‚½: " + flickGesture.ScreenFlickVector );
+    }
 }
