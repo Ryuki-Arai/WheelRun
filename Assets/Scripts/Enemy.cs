dc5b4252e4762d12eed2982ab0672128d28ae67c
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2022-11-14 10:54:03 +0900

    Fixed Enemy Object

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,36 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Enemy : ObjectBase
 {
+    [SerializeField] TMP_Text _levelText;
     [SerializeField] public event Action<int> Damage;
     [SerializeField] public event Action<int> scoreUP;
+    int level;
     [SerializeField] int damage;
     [SerializeField] int score;
+
+    private void Start()
+    {
+        _levelText.GetComponents<TMP_Text>();
+        level = UnityEngine.Random.Range(1, 10);
+        _levelText.text = level.ToString();
+    }
     public override void Action()
     {
-        Damage += GameManager.Player.Damage;
-        Damage.Invoke(damage);
-        Destroy(gameObject);
+        if(GameManager.Player.Level < level)
+        {
+            Damage += GameManager.Player.Damage;
+            Damage.Invoke(damage);
+        }
+        else
+        {
+            scoreUP += GameManager.Player.ScoreUP;
+            scoreUP.Invoke(score);
+            Destroy(gameObject);
+        }
     }
 }
